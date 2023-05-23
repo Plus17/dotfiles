5dcae9918dc5b6143eafe901b761fd1a2bb285ba
Sergio <me@sergiovp.dev>
2023-05-23 16:23:54 -0600

    feat: move nvim conf

---
+++ nvim/.config/nvim/_after/plugin/colors.lua
@@ -0,0 +1,5 @@
+function UseColorScheme()
+  vim.cmd([[colorscheme "catppuccin"]])
+end
+
+UseColorScheme()
---
+++ nvim/.config/nvim/lua/plus17/bufferline.lua
@@ -0,0 +1,5 @@
+vim.opt.termguicolors = true
+require("bufferline").setup()
+
+vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
